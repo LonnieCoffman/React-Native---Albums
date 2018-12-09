cc71e0e3282f9791ca4eaa9581f49663035dee47
LonnieCoffman <LonnieCoffman@users.noreply.github.com>
2018-12-08 17:17:53 -0800

    Added button text props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = ({ onPress }) => {
+const Button = ({ onPress, buttonText }) => {
   const {  buttonStyle, textStyle } = styles;
 
   return (
@@ -9,7 +9,7 @@ const Button = ({ onPress }) => {
       onPress={ onPress }
       style={ buttonStyle } >
       <Text style={ textStyle }>
-        Click Me!!!
+        {buttonText}
       </Text>
     </TouchableOpacity>
   );
